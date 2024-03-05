065a3351db9b2b1e0a3fdbbd58a842b757f89170
FathmaIrshad <fathma.irshad@gmail.com>
2024-03-05 10:15:24 +0400

    Adding Reviews Model

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,29 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema; //shortcut to reference to mongoose Schema
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [    //array of object ID to store 
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'     //object Id to access from review model
+        }
+    ]
 });
 
-module.exports=mongoose.model('Campground', CampgroundSchema);
+CampgroundSchema.post('findOneAndDelete', async function (doc) {   //Delete a campground and the related reviews both from webpage and from db using Mongoose middleware
+    if (doc) {                      //if a camground exists delete the reviews if the its id in present in doc.reviews. doc refers to the deleted content
+        await Review.deleteMany({     
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
-const mongoose=require('mongoose');
-const cities= require('./cities');
-const {places, descriptors}=require('./seedHelpers');
-const Campground= require('../models/campground');
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
 //we can run this file separate from node to seed the database
 mongoose.connect('mongodb://0.0.0.0:27017/yelp-camp')
@@ -12,7 +12,6 @@ mongoose.connect('mongodb://0.0.0.0:27017/yelp-camp')
 // });
     //use local development or prduction database
     
-const sample= array => array[Math.floor(Math.random() * array.length)];
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:')); 
@@ -20,21 +19,25 @@ db.once('open', () => {
     console.log("Database connected");  //if succesfully opened, display this message
 });
 
-//to create new campgrounds with random.city and random.location from cities.js
-const seedDB= async() =>{
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i=0; i<50; i++){
-        const random1000=Math.floor(Math.random() *1000);
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
         })
         await camp.save();
     }
- }
+}
 
-//to give title in Schema which is random.descriptor+random.place
-//to run in terminal, >>node seeds/index.js
-seedDB().then(() =>{
+seedDB().then(() => {
     mongoose.connection.close();
-});
+})
